3e6edf799ed7bdba3175bcca611431a83d2c57c8
Girish Latake <147689255+giiriish@users.noreply.github.com>
2026-03-28 13:38:38 +0530

    Enhance variable definitions in variables.tf
    Added descriptions and types for existing variables in variables.tf.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,29 @@
-variable "vpc_id" {}
-variable "public_subnet_id" {}
-variable "private_subnet_id" {}
-variable "key_name" {}
-variable "ami_id" {}
-variable "my_ip" {}
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Public Subnet ID (for Web Tier)"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "Private Subnet ID (for App Tier)"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair Name"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your laptop public IP for SSH access"
+  type        = string
+}
