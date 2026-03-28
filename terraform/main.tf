5bfd428d4db7afc989a2366ed1f32277a754cba5
Girish Latake <147689255+giiriish@users.noreply.github.com>
2026-03-28 17:22:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,23 @@
+variable "vpc_id" {}
+variable "vpc_cidr" {
+  default = "10.0.0.0/22"
+}
 
-#  External ALB SG
-
+# External ALB SG
 
 resource "aws_security_group" "external_alb_sg" {
-  name   = "external-alb-sg"
-  vpc_id = var.vpc_id
+  name        = "external-alb-sg"
+  description = "Allow HTTP from internet"
+  vpc_id      = var.vpc_id
 
   ingress {
+    description = "HTTP from anywhere"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -29,40 +27,28 @@ resource "aws_security_group" "external_alb_sg" {
 }
 
 
-#  Internal ALB SG
+# Web SG
 
-
-resource "aws_security_group" "internal_alb_sg" {
-  name   = "internal-alb-sg"
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
   vpc_id = var.vpc_id
 
+  # HTTP from External ALB
   ingress {
+    description     = "HTTP from External ALB"
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.external_alb_sg.id]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-#  Web Tier SG
-
-resource "aws_security_group" "web_sg" {
-  name   = "web-tier-sg"
-  vpc_id = var.vpc_id
-
+  # HTTP from VPC
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.internal_alb_sg.id]
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
@@ -74,18 +60,18 @@ resource "aws_security_group" "web_sg" {
 }
 
 
-#  App Tier SG
-
+# App SG
 
 resource "aws_security_group" "app_sg" {
-  name   = "app-tier-sg"
+  name   = "app-sg"
   vpc_id = var.vpc_id
 
   ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
+    description = "App port"
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
@@ -97,18 +83,41 @@ resource "aws_security_group" "app_sg" {
 }
 
 
-#  Database SG
+# Internal ALB SG
+
+resource "aws_security_group" "internal_alb_sg" {
+  name   = "internal-alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
-resource "aws_security_group" "database_sg" {
+# Database SG
+
+resource "aws_security_group" "db_sg" {
   name   = "database-sg"
   vpc_id = var.vpc_id
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app_sg.id]
+    description = "MySQL access"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
