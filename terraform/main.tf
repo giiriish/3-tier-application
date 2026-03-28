65425bae1bad1c2a6ac3900077d3774075062c79
Girish Latake <147689255+giiriish@users.noreply.github.com>
2026-03-28 20:42:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,12 +64,22 @@ resource "aws_security_group" "app_sg" {
   name   = "app-sg"
   vpc_id = var.vpc_id
 
+  
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # temporary
+  }
+
+  # App port
   ingress {
     description = "App port"
     from_port   = 4000
     to_port     = 4000
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
