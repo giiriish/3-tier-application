f96d8fea79741bed995babcb80ed07d3f652a68e
Girish Latake <147689255+giiriish@users.noreply.github.com>
2026-03-30 19:09:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,152 +1,72 @@
-########################################
-# EXTERNAL ALB SG
-########################################
-
-resource "aws_security_group" "external_alb_sg" {
-  name_prefix = "external-alb-sg-"
-  vpc_id      = data.aws_vpc.existing_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
-  }
-  tags = {
-    Name = "external-alb-sg"
-  }
+# External ALB → Internet
+resource "aws_security_group_rule" "alb_http" {
+  type              = "ingress"
+  security_group_id = aws_security_group.external_alb_sg.id
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-
-########################################
-# WEB TIER SG
-########################################
-
-resource "aws_security_group" "web_sg" {
-  name_prefix = "web-tier-sg-"
-  vpc_id      = data.aws_vpc.existing_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.external_alb_sg.id]
-  }
-
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.external_alb_sg.id]
-  }
-
-  # To Internal LB
-  egress {
-    from_port       = 4000
-    to_port         = 4000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.internal_alb_sg.id]
-  }
-
-  tags = {
-    Name = "web-tier-sg"
-  }
+resource "aws_security_group_rule" "alb_https" {
+  type              = "ingress"
+  security_group_id = aws_security_group.external_alb_sg.id
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-########################################
-# INTERNAL ALB SG
-########################################
-
-resource "aws_security_group" "internal_alb_sg" {
-  name_prefix = "internal-alb-sg-"
-  vpc_id      = data.aws_vpc.existing_vpc.id
-
-  ingress {
-    from_port       = 4000
-    to_port         = 4000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
-  }
-
- # To App Tier
-  egress {
-    from_port       = 4000
-    to_port         = 4000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app_sg.id]
-  }
-
-  tags = {
-    Name = "internal-alb-sg"
-  }
+# ALB → WEB
+resource "aws_security_group_rule" "alb_to_web" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.web_sg.id
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.external_alb_sg.id
 }
-########################################
-# APP TIER SG
-########################################
 
-resource "aws_security_group" "app_sg" {
-  name_prefix = "app-tier-sg-"
-  vpc_id      = data.aws_vpc.existing_vpc.id
-
-  ingress {
-    from_port       = 4000
-    to_port         = 4000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.internal_alb_sg.id]
-  }
-
-   # To DB
-  egress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.db_sg.id]
-  }
-
-  tags = {
-    Name = "app-tier-sg"
-  }
+# SSH to WEB
+resource "aws_security_group_rule" "ssh_web" {
+  type              = "ingress"
+  security_group_id = aws_security_group.web_sg.id
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip]
 }
-########################################
-# DATABASE SG
-########################################
 
-resource "aws_security_group" "db_sg" {
-  name_prefix = "db-tier-sg-"
-  vpc_id      = data.aws_vpc.existing_vpc.id
-
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app_sg.id]
-  }
-
-  egress {
-    from_port       = 4000
-    to_port         = 4000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app_sg.id]
-  }
-
-  tags = {
-    Name = "app-tier-sg"
-  }
+# WEB → INTERNAL ALB
+resource "aws_security_group_rule" "web_to_internal_alb" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.internal_alb_sg.id
+  from_port                = 4000
+  to_port                  = 4000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.web_sg.id
 }
+
+# INTERNAL ALB → APP
+resource "aws_security_group_rule" "internal_alb_to_app" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.app_sg.id
+  from_port                = 4000
+  to_port                  = 4000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.internal_alb_sg.id
+}
+
+# APP → DB
+resource "aws_security_group_rule" "app_to_db" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.db_sg.id
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
 
 ########################################
 # WEB INSTANCE
