49519b79386fb73e41391c248383a3890e143736
Girish Latake <147689255+giiriish@users.noreply.github.com>
2026-04-05 00:04:48 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,7 @@ output "web_public_ip" {
   value = aws_instance.web.public_ip
 }
 
-output "app_private_ip" {
-  value = aws_instance.app.private_ip
+output "app_instance_id" {
+  value = aws_instance.app.id
 }
 
