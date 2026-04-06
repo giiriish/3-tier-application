d84dccd698f1216f66d5cf47e7d5e0d9cd2cc6b6
Girish Latake <147689255+giiriish@users.noreply.github.com>
2026-04-06 23:33:54 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "guru-3-tier"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
